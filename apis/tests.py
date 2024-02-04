a9051d5f24b0efc9e4636ce26e9b3299eb5d794a
mosleh <moslehazizi@gmail.com>
2024-02-04 19:56:04 +0330

    add test and update requirements.txt

--- apis/tests.py
+++ apis/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from books.models import Book
 
-# Create your tests here.
+class APITests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title = "my test book",
+            subtitle = "my test subtitle of test book",
+            author = "test author",
+            isbn = "1234567899876",
+        )
+    
+    def test_api_listview(self):
+        response = self.client.get(reverse("book_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Book.objects.count(), 1)
+        self.assertContains(response, self.book)
